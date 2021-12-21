c53987af144a849b067d5dbfaa1c02470b6e50af
Misken <miskens@hotmail.com>
2021-12-21 21:52:17 +0100

    Added the base for Game.java.

--- src/MemoryGame/Game.java
+++ src/MemoryGame/Game.java
@@ -1,6 +1,15 @@
 package MemoryGame;
-public class Game {
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game extends Application {
     public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
         
     }
 }
