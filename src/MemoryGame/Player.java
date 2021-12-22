01c4296f4d75116512ac4325a30b09914e46bb56
theo <thoeodurs@dejager.net>
2021-12-22 19:47:06 +0100

    Remade Player class

--- src/MemoryGame/Player.java
+++ src/MemoryGame/Player.java
@@ -1,9 +1,16 @@
 package MemoryGame;
-
 public class Player {
     private String playerName;
+    private int points;
+    private boolean active;  
 
-    public Player() {
-        
+    public Player(String name){
+        this.playerName =  name;
+        this.points = 0;
+        this.active = false; //I spellogiken hanterar vi vem som börjar (random)
     }
-}
+
+    public String getPlayerName(){
+        return playerName;
+    }
+//"vmArgs": "--module-path /home/theo/Hämtningar/openjfx-17.0.1_linux-aarch64_bin-sdk/javafx-sdk-17.0.1/lib/ --add-modules javafx.controls,javafx.fxml",
