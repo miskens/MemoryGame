d7a8055eaf4a78a0cc7c4769a2d8cda970789409
Theodorus De Jager <theodorus.dejager@learnet.se>
2021-12-23 12:12:48 +0100

    changes made to player class

--- src/MemoryGame/Player.java
+++ src/MemoryGame/Player.java
@@ -1,16 +1,30 @@
 package MemoryGame;
 public class Player {
-    private String playerName;
-    private int points;
-    private boolean active;  
+    private final String playerName;
+    private int pPoints;
+    private boolean pActive;  
 
     public Player(String name){
         this.playerName =  name;
-        this.points = 0;
-        this.active = false; //I spellogiken hanterar vi vem som börjar (random)
+        this.pPoints = 0;
+        this.pActive = false; //I spellogiken hanterar vi vem som börjar (random)
     }
 
     public String getPlayerName(){
         return playerName;
     }
 
+    /*public void setPlayerName(String name){
+        this.playerName = name;
+
+    }*/
+    public boolean getActive(){
+        return pActive;
+    }
+    
+    public int getpPoints() {
+        return pPoints;
+    }
+}
+//active
+//points
