c38ecc4b5a56f1cebed6c913714d155caac53c91
Misken <miskens@hotmail.com>
2021-12-28 09:22:57 +0100

    Added Test case 1: testNPlayerNumberIncrementCorrectly()

--- src/Test/GameTest.java
+++ src/Test/GameTest.java
@@ -1,5 +1,27 @@
 package Test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import MemoryGame.Player;
+
 public class GameTest {
 
+    @Test
+    public void testNPlayerNumberIncrementCorrectly() {
+        // Setup
+        Player[] players = {
+            new Player("player 1"),
+            new Player("player 2"),
+            new Player("player 3")
+            };
+
+        // Actual
+        int actual = players[2].getPlayerNr();
+
+        // Expected
+        int expected = 3;
+
+        // Assert is true
+        Assert.assertEquals(expected, actual);
+    }
 }
