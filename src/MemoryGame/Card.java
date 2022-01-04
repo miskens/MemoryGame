7d15bbda42c987df03d696da7c26b81f7691b2c2
Renas Said <renas.said@live.com>
2022-01-04 11:42:25 +0100

    Removed visibility enum & unused imports

--- src/MemoryGame/Card.java
+++ src/MemoryGame/Card.java
@@ -3,29 +3,18 @@ package MemoryGame;
 import javafx.scene.image.Image;
 
 public class Card extends Image {
-    private Visibility visibility;
     private String visibleCardSource;
     private final String invisibleCardSource;
 
     public Card(String imageSource) {
         super(imageSource);
-        this.visibility = Visibility.VISIBLE;
         this.invisibleCardSource = "Icons/turned.png";
     }
 
     public Card(String imageSource, String visibleCardSource) {
         super(imageSource);
-        this.visibleCardSource = visibleCardSource;
-        this.visibility = Visibility.INVISIBLE; 
+        this.visibleCardSource = visibleCardSource; 
         this.invisibleCardSource = "Icons/turned.png";
-    }
-
-    public Visibility getVisibility() {
-        return this.visibility;
-    }
-
-    public void setVisibility(Visibility visibility) {
-        this.visibility = visibility;
     }
 
     public String getInvisibleCardSource() {
--- src/MemoryGame/Visibility.java
+++
@@ -1,5 +0,0 @@
-package MemoryGame;
-
-public enum Visibility {
-    VISIBLE, INVISIBLE, ELIMINATED
-}
